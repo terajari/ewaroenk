003321163d69008ff720c54b3c0a478d8730347d
agusanggarwanto <agusanggarwanto@gmail.com>
2023-11-20 08:58:08 +0700

    project init

---
+++ src/main/java/com/terajari/ewaroenk/EwaroenkApplication.java
@@ -0,0 +1,13 @@
+package com.terajari.ewaroenk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwaroenkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwaroenkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terajari/ewaroenk/EwaroenkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terajari.ewaroenk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwaroenkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
